8412c158a03841b6d5063f163d1c813bcd804f3b
ksk-uchi <ksk.uchi+github@gmail.com>
2017-09-26 00:07:59 +0900

    Adding README

---
+++ README.md
@@ -0,0 +1,8 @@
+# My Elasticsearch practicing repo
+
+## Run this
+
+```
+docker-compose up --build -d
+```
+
--- runES.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Only production
-# sudo sysctl -w vm.max_map_count=262144
-docker-compose up --build
-
